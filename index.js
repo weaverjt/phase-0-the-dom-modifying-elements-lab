9f3c9164836e57e2d14439eede71e20b02346668
Jacob <weaver.jacob.t@gmail.com>
2023-07-16 20:13:24 -0500

    cats

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const re = document.getElementById("main");
+re.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Jake is the champion";
+document.body.append(newHeader);
